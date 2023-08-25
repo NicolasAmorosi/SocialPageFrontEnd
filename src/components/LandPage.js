2c3891598d89df0a4a33f85f0b3adf57dc66b028
NicolasAmorosi <nicolasamorosi@gmail.com>
2023-08-25 11:23:27 -0300

    nativewind and navigation add

---
+++ src/components/LandPage.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text, View } from 'react-native'
+
+export default function LandPage() {
+  return (
+    <SafeAreaView>
+      <View className=" items-center justify-center h-2/6 bg-slate-300">
+        <Text className=" text-3xl font-bold text-black">Hola que tal</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
