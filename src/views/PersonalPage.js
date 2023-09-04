a7626bed463d71762dcae9a46c3653b50d0cea25
NicolasAmorosi <nicolasamorosi@gmail.com>
2023-09-04 11:41:51 -0300

    PersonalPage fix

--- src/views/PersonalPage.js
+++ src/views/PersonalPage.js
@@ -1,30 +1,26 @@
 import { View, Text, Image, SafeAreaView, ScrollView } from 'react-native';
 import TopBar from '../components/TopBar';
 
-export default function PersonalPage(){
-  return(
-    <SafeAreaView className=" flex">
+export default function PersonalPage() {
+  return (
+    <SafeAreaView className="">
       <TopBar name="PersonalPage" />
-      <View className=" flex flex-row space-x-4 w-screen h-auto p-4 items-center">
-        <Image source={require('../../assets/profilePhoto.jpg')} className=" rounded-full h-20 w-20"/>
+
+
+      <View className=" flex flex-row space-x-4 p-4 items-center">
+        <Image source={require('../../assets/profilePhoto.jpg')} className=" rounded-full h-20 w-20" />
         <Text className=" text-xl">Nicolas Amorosi</Text>
       </View>
-      <View className=" p-4 w-screen h-auto">
+      <View className=" p-4">
         <Text className=" text-xl">Description</Text>
       </View>
-      <ScrollView className=" w-screen h-auto flex flex-wrap">
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
-        <Image source={require('../../assets/post.jpg')} className=" w-32 h-32"/>
+      <ScrollView className="">
+        <View className="flex-wrap flex-1 flex-row p-2 gap-2">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((item, index) => (
+            <Image key={index} source={require('../../assets/post.jpg')} className=" w-[120] h-[120]" />
+          ))}
+        </View>
       </ScrollView>
-    </SafeAreaView>
+    </SafeAreaView >
   )
 }
